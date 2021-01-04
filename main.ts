c88449086d0923ea0d062241fb7236e2a3593769
bruclemente <76972776+bruclemente@users.noreply.github.com>
2021-01-04 22:01:05 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,43 @@
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # # .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        . . # . .
+        `)
+}
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # . # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+}
+images.createBigImage(`
+    # . . # . . . . . #
+    . # # . . . # # # #
+    . # # # # # # # . .
+    . # # . . . # # # #
+    # . . # . . . . . #
+    `).scrollImage(1, 200)
+basic.showString(" Bon Nadal !!!")
 basic.forever(function () {
 	
 })
